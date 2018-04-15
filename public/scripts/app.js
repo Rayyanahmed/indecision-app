cd9bd9987e3a43c89cee4d1d3a8314ff572fa3e3
Rayyan Ahmed <rayyan@Rayyans-MacBook-Air.local>
2018-04-15 11:56:33 -0400

    set up yarn to generate JSX into js code browser can understand

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,13 @@
+'use strict';
+
 console.log('App.js is running');
 
 // JSX - Javascript XML
 var template = React.createElement(
-    "h1",
-    { id: "someid" },
-    "This is JSX from app.js"
-  );
+  'h1',
+  null,
+  'Indecision App'
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);

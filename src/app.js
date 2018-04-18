f2b6c68e61a3409553eca98ae9d71d0584511251
Rayyan Ahmed <rayyan@Rayyans-MacBook-Air.local>
2018-04-17 22:42:46 -0400

    Doing challenge #1: Rendering template2 instead of template1

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
 console.log('App.js is running');
 
 // JSX - Javascript XML
-var template = <h1>Indecision App</h1>;
+var template = (
+<div>
+    <h1>Indecision App</h1>
+    <p>This is some info</p>
+    <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+    </ol>
+</div>
+)
+;
+
+// Create a templateTwo var JSX expression
+// div
+
+
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
